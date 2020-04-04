bfacd2c03b52f1ddca33afb7a89540b6e6dc7261
Cory Sandlin <corybsa@gmail.com>
2020-04-04 21:17:29 +0000

    figuring out how to run this in a container

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,26 @@
 int main(int argc, char* args[]) {
     GameBoy* gameboy = new GameBoy();
 
-    // SDL_Window* window = NULL;
-    // SDL_Surface* screenSurface = NULL;
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
 
-    // if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-    //     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    // } else {
-    //     window = SDL_CreateWindow("GoodBoy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("GoodBoy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 256, SDL_WINDOW_SHOWN);
 
-    //     if(window == NULL) {
-    //         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    //     } else {
-    //         screenSurface = SDL_GetWindowSurface(window);
-    //         SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-    //         SDL_UpdateWindowSurface(window);
-    //         SDL_Delay(2000);
-    //     }
-    // }
+        if(window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
+        }
+    }
 
-    // SDL_DestroyWindow(window);
-    // SDL_Quit();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 
 
