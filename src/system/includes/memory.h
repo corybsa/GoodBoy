3a594c3f70f09a3eb7446f3d76c7950f6d7d3d0c
Cory Sandlin <corybsa@gmail.com>
2020-04-05 21:06:17 -0400

    moved header files to their owo folder

---
+++ src/system/includes/memory.h
@@ -0,0 +1,79 @@
+#ifndef GOODBOY_MEMORY_H
+#define GOODBOY_MEMORY_H
+
+#include "types.h"
+#include <functional>
+
+/**
+ * The GameBoy has 64KB of Memory.
+ *
+ * Notes:
+ *   - Address 0xE000 - 0xFE00 appear to access the internal RAM the same as 0xC000 - 0xDE00.
+ *     i.e. if you write to 0xE000 it will appear at 0xC000 AND 0xE000. Similarly, writing to 0xC000 will appear at 0xC000 AND 0xE000.
+ *
+ *   - Memory regions:
+ *     - 0x0000 - 0x3FFF: ROM (16KB ROM bank #0 (in cartridge))
+ *     - 0x4000 - 0x7FFF: ROM (16KB switchable ROM bank (in cartridge))
+ *                        - I think these can safely be combined for a range of 0x0000 - 0x7FFF
+ *
+ *     - 0x8000 - 0x9FFF: VRAM (8KB Video RAM)
+ *                        - Can only be accessed during LCD modes 0 (HBlank), 1 (VBlank) and 2 (OAM Search)
+ *
+ *     - 0xA000 - 0xBFFF: SRAM (8KB switchable RAM bank)
+ *                        - Most of the time this is used for save data as it's backed up by a battery.
+ *                        - Can be used as extra work RAM.
+ *                        - By default, SRAM is in a locked state, which ignores writes and returns somewhat random values when read.
+ *                        - How SRAM is unlocked depends on the MBC.
+ *
+ *     - 0xC000 - 0xDFFF: WRAM (8KB internal RAM)
+ *                        - This is physically in the GameBoy itself and can be used however the programmer wants.
+ *
+ *     - 0xE000 - 0xFDFF: ERAM (Echo of 8KB Internal RAM)
+ *                        - Basically points to 0xC000 - 0xDE00
+ *                        - It's recommended to avoid relying on this though.
+ *
+ *     - 0xFE00 - 0xFE9F: OAM (Object Attribute Memory or Sprite Attribute Memory)
+ *                        - The area of memory where information about objects is stored.
+ *                        - Locked while the PPU is accessing it.
+ *
+ *     - 0xFEA0 - 0xFEFF: Empty but unusable for I/O
+ *
+ *     - 0xFF00 - 0xFF4B: IO (I/O Ports)
+ *                        - A bunch of hardware registers.
+ *                        - Here is where you can configure graphics, play sound or communicate with another GameBoy.
+ *
+ *     - 0xFF4C - 0xFF7F: Empty but unusable for I/O
+ *
+ *     - 0xFF80 - 0xFFFE: HRAM (Internal RAM or High RAM)
+ *                        - These bytes work just like WRAM, except that they can be accessed slightly faster by a certain instruction.
+ *                        - This is a great place to store temporary variables because of the speed.
+ *
+ *     - 0xFFFF         : IE (Interrupt Enable Register)
+ *                        - Special byte of I/O.
+ *                        - It's here because of how the CPU works internally.
+ */
+class Memory {
+private:
+    int cartridge[0x800000];
+    int vram[0x2000];
+    int sram[0x8000];
+    int wram[0x2000];
+    int eram[0x1E00];
+    int oam[0xA0];
+    int fea0_feff[0x60];
+    int io[0x80];
+    int hram[0x7F];
+    int ie[0x01];
+
+    std::function<void(word)> gpuCallback;
+
+public:
+    Memory();
+
+    byte getByteAt(word address);
+    void setByteAt(word address, byte value);
+
+    void setGpuCallback(std::function<void(word)> arg2);
+};
+
+#endif
---
+++ src/system/includes/types.h
@@ -0,0 +1,7 @@
+#ifndef GOODBOY_TYPES_H
+#define GOODBOY_TYPES_H
+
+typedef unsigned char byte;
+typedef unsigned short word;
+
+#endif
